19bd1f0441417dbf7b541c6e324f04ae47bfdb9f
serii burduja <seriiburduja@gmail.com>
2024-12-13 09:09:35 +0200

    feat: services page and service block

---
+++ components/Service.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div class="service">
+    <div class="service__icon">
+      <div class="service__circle"></div>
+      <slot></slot>
+    </div>
+    <h3 class="service__title">{{ title }}</h3>
+    <div class="service__text">
+      <p>{{ text }}</p>
+    </div>
+    <Btn>
+      <a target="_blank" :href="url">Читать далее...</a>
+    </Btn>
+  </div>
+</template>
+<style lang="scss">
+@keyframes rotateX {
+  0% {
+    transform: rotateY(0);
+  }
+  100% {
+    transform: rotateY(360deg);
+  }
+}
+.service {
+  text-align: center;
+  &:hover {
+    .service__icon {
+      background-color: #222;
+      box-shadow: 0 6px 16px -4px rgba(0, 0, 0, 0.3);
+      img {
+        animation: rotateX 2s infinite linear forwards;
+      }
+    }
+  }
+  &__icon {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto 3rem;
+    width: 12rem;
+    height: 12rem;
+    overflow: hidden;
+    border-radius: 50%;
+    transition: all 0.6s;
+  }
+  &__circle {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 1px solid #444;
+    border-radius: 50%;
+  }
+  &__title {
+    margin-bottom: 2rem;
+    font-size: 2.8rem;
+    font-weight: 600;
+    color: #fff;
+  }
+}
+</style>
--- pages/services.vue
+++ pages/services.vue
@@ -1,1 +1,47 @@
-<template></template>
+<script setup lang="ts">
+import services_text from "~/text/services";
+const runtimeConfig = useRuntimeConfig();
+useHead({
+  title: runtimeConfig.public.site_title + " | Услуги",
+  meta: [
+    {
+      hid: "description",
+      name: "description",
+      content:
+        "Vue (произносится /vjuː/, примерно как view) — это прогрессивный фреймворк для создания пользовательских интерфейсов. В отличие от фреймворков-монолитов",
+    },
+  ],
+});
+</script>
+<template>
+  <div class="page-services">
+    <section-header title="Мои преимущества" subtitle="Услуги" />
+    <div class="page-services__wrap">
+      <div
+        class="page-services__item"
+        v-for="{ title, text, icon, url } in services_text"
+        :key="title"
+      >
+        <Service :url="url" :title="title" :text="text">
+          <img :src="icon" alt="" />
+        </Service>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+.page-services {
+  &__wrap {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(40rem, 1fr));
+    grid-gap: 8rem;
+    padding: 8rem;
+  }
+  &__item {
+    padding: 4rem;
+    box-shadow: 0 4px 14px -2px rgba(0, 0, 0, 0.4);
+    border-radius: 1rem;
+    overflow: hidden;
+  }
+}
+</style>
